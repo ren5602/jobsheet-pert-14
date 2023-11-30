aa05885686e41992d9ee10a469c56852eb95f156
ren5602 <naziz5602@gmail.com>
2023-11-30 15:21:55 +0700

    percobaan 2 hitung pangkat

---
+++ Percobaan2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Percobaan2 {
+    static int hitungPangkat(int x, int y){
+        if (y == 0){
+            return 1;
+        }else{
+            return x * hitungPangkat(x, y-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("bilangan yang dihitung: ");
+        int bilangan = sc.nextInt();
+        System.out.print("pangkat: ");
+        int pangkat = sc.nextInt();
+        System.out.println(hitungPangkat(bilangan, pangkat));
+    }
+}
